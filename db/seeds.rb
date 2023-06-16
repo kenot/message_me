031c8e714810c3503c15dfd2e4794e5cac86c448
tdinkov <tdinkov@cisco.com>
2023-06-16 17:32:18 +0300

    messageme is ready

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Mashrur", password: "Password2")
+User.create(username: "JeanMichel", password: "Password3710")
+User.create(username: "Lubina", password: "Password4mgt1")
+User.create(username: "Didier", password: "PasswordFuk3451")
+User.create(username: "Yves", password: "Password7FuCk67")
